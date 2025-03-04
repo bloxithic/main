a535398513fb05c124eb0dd15b9891baf215a930
bloxithic <bloxithic@gmail.com>
2025-03-04 21:42:48 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,4 +2,4 @@ if game.PlaceId ~= 13864667823 then
 	if game.PlaceId == 14775231477 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/BreakIn2.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/Universal"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/Universal.lua"))()
