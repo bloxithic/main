99c5b6c0ba9f02b882b44ae5578c40dc62088dde
bloxithic <bloxithic@gmail.com>
2025-03-06 21:44:47 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,11 @@
-if game.PlaceId ~= 13864667823 then
-	if game.PlaceId == 14775231477 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/BreakIn2.lua"))()
-else
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/Universal.lua"))()
+local Games = {
+	[14775231477] = "https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/BreakIn2.lua", -- Break In 2
+}
+return Games
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+	else
+		loadstring(game:HttpGet(https://raw.githubusercontent.com/bloxithic/notepad/refs/heads/main/Universal.lua))()
+end
