db70077aa94f5b943ec9861db6558f53c628cfa6
bloxithic <bloxithic@gmail.com>
2025-03-09 16:39:28 +0700

    Create BloxFruits.lua

---
+++ BloxFruits.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "notepad", SaveConfig = false, ConfigFolder = "Blox Fruits"})
+
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998"
+})
