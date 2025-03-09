be814912433cf46de9789abc3764c9e72677eb51
bloxithic <bloxithic@gmail.com>
2025-03-09 12:53:17 +0700

    Update BreakIn2.lua

--- BreakIn2.lua
+++ BreakIn2.lua
@@ -4,11 +4,36 @@ local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
 -- Functions:
-local function getHumanoid()
-    local character = game.Players.LocalPlayer.Character
-    return character and character:FindFirstChildOfClass("Humanoid")
+local function updateHumanoid()
+    humanoid = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+    if humanoid then
+        originalWalkSpeed = humanoid.WalkSpeed
+        originalJumpPower = humanoid.JumpPower
+    end
 end
 
+game.Players.LocalPlayer.CharacterAdded:Connect(updateHumanoid)
+updateHumanoid()
+
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
+end
 -- Main code/GUI
 
 local Window = Library:CreateWindow({
